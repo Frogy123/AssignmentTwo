1cefa67b16b3ac5e626a5e99e992270ba2e3d031
David <davidpaster2018@gmail.com>
2025-01-01 20:14:23 +0200

    finished merging David + Eithan parts

--- src/main/java/bgu/spl/mics/application/objects/DetectedObject.java
+++ src/main/java/bgu/spl/mics/application/objects/DetectedObject.java
@@ -5,6 +5,30 @@ package bgu.spl.mics.application.objects;
  * It contains information such as the object's ID and description.
  */
 public class DetectedObject {
+	private String id; // The ID of the object.
+	private String description; // Description of the object.
 
-    // TODO: Define fields and methods.
+	// Constructor
+	public DetectedObject(String id, String description) {
+		this.id = id;
+		this.description = description;
+	}
+
+	// Getters
+	public String getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	// Setters
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- src/main/java/bgu/spl/mics/application/services/CameraService.java
+++ src/main/java/bgu/spl/mics/application/services/CameraService.java
@@ -1,24 +1,31 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.Broadcast;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.CrashedBroadcast;
+import bgu.spl.mics.application.messages.TerminatedBroadcast;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.objects.Camera;
 
 /**
  * CameraService is responsible for processing data from the camera and
  * sending DetectObjectsEvents to LiDAR workers.
- * 
+ *
  * This service interacts with the Camera object to detect objects and updates
  * the system's StatisticalFolder upon sending its observations.
  */
 public class CameraService extends MicroService {
+    private static int cameraServiceCount = 0;
 
+    private Camera camera;
     /**
      * Constructor for CameraService.
      *
      * @param camera The Camera object that this service will use to detect objects.
      */
     public CameraService(Camera camera) {
-        super("Change_This_Name");
-        // TODO Implement this
+        super("CameraService" + cameraServiceCount++);
+        this.camera = camera;
     }
 
     /**
@@ -28,6 +35,15 @@ public class CameraService extends MicroService {
      */
     @Override
     protected void initialize() {
-        // TODO Implement this
+        this.subscribeBroadcast(TickBroadcast.class, this::detectObj);
+        this.subscribeBroadcast(TerminatedBroadcast.class,boradcast -> terminate());
+        this.subscribeBroadcast(CrashedBroadcast.class,boradcast -> terminate());
+
+
+    }
+    private void detectObj(TickBroadcast t) {
+        // parse json file
+        // see what objects are detected in t.time + f
+        // send detected object event
     }
 }
